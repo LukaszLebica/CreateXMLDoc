b8e5c8acc08ebf7b2224cd06e9808821e1cb056f
Łukasz Lebica <lukasz.lebica.835@gmail.com>
2020-01-11 17:17:49 +0100

    Add project files.

---
+++ CreateXMLDoc/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CreateXMLDoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            xmlDocument xml = new xmlDocument();
+        }
+    }
+}
---
+++ CreateXMLDoc/xmlDocument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace CreateXMLDoc
+{
+    class xmlDocument
+    {
+        public xmlDocument()
+        {
+            using(XmlWriter writer = XmlWriter.Create("document.xml"))
+            {
+                writer.WriteStartElement("document");
+                writer.WriteElementString("title", "The XML document creation.");
+                writer.WriteEndElement();
+                writer.Flush();
+            }
+        }
+
+    }
+}
+\repos\CreateXMLDoc
